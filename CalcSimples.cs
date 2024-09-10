ad4f064b7aacd56c9f491579c10d5505c0079598
Nilson Jose <nilsonjts@gmail.com>
2024-09-10 12:28:11 -0300

    first commit

---
+++ CalcSimples.cs
@@ -0,0 +1,43 @@
+﻿/*
+
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ex2;
+
+public class CalcSimples
+{
+
+    public double A=0;
+    public double B=0;
+    public double C=0;
+    
+    public CalcSimples()
+    {
+    }
+
+    public void LeituraDados(){
+        
+        Console.WriteLine("Coloque o valor de A: ");
+        A=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Coloque o valor de B ");
+        B=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Coloque o valor de C: ");
+        C=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write(Calcular());
+    }
+
+    public double Calcular(){
+
+        double S= Math.Pow( this.B + this.C , 2);
+        double R= Math.Pow( this.A + this.B , 2);
+        double D= (R+S)/2;
+
+        return D;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ex2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CalcSimples obj = new CalcSimples();
+        obj.LeituraDados();
+    }
+}
